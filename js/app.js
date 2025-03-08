bbdcc7be0223bd767d0941d205ee63ee720ff4cf
TorresMariana <mt.grafico@gmail.com>
2025-03-08 14:31:46 -0300

    funcion condicionesiniciales

--- js/app.js
+++ js/app.js
@@ -9,44 +9,29 @@ let listaNombresSorteados = [];
 // Desarrolla una función, que permita al usuario ingresar un nombre en 
 // el campo de texto y añadirlo a la lista de amigos creada anteriormente.
 function agregarAmigo(){
-    // Capturar el valor del campo de entrada: Utilizar document.getElementById o 
-    // document.querySelector para obtener el texto ingresado por el usuario.
+    // Capturar el valor del campo de entrada
     let nombre = document.getElementById('amigo').value;
-    console.log(nombre);
-
+    
     //validar la entrada
-    validarEntrada(nombre);
-    // if(nombre == ''){
-    //     alert('Por favor, inserte un nombre.');
-    // }else{
-    //     // Actualizar el array de amigos
-    //     amigos.push(nombre);
-    //     // Limpiar el campo de entrada
-    //     document.querySelector('#amigo').value = '';
-    //     actualizarListaDeAmigos();
-    // }
-
-    console.log(amigos);
+    if(validarEntrada(nombre)){
+        actualizarListaDeAmigos();
+        condicionesInciales();
+        return true;
+    }
 }
 
 function validarEntrada(nombreIngresado){
     if(nombreIngresado == ''){
         alert('Por favor, inserte un nombre.');
+        return false;
     }else{
+        console.log(nombreIngresado);
         // Actualizar el array de amigos
         amigos.push(nombreIngresado);
-        // Limpiar el campo de entrada
-        document.querySelector('#amigo').value = '';
         indiceMaximo++;
-        actualizarListaDeAmigos();
+        return true;
     }
 }
-
-
-function limpiarLista(idLista){
-    return document.querySelector(`#${idLista}`).innerHTML = "";
-}
-
 
 // Crea una función que recorra el array amigos y agregue cada nombre como 
 // un elemento <li> dentro de una lista HTML. Usa innerHTML para limpiar la 
@@ -57,14 +42,13 @@ function actualizarListaDeAmigos(){
 
     // Limpiar la lista existente
    limpiarLista('listaAmigos');
-    if(amigos.length > 0){
+    if(indiceMaximo > 0){
         // Iterar sobre el arreglo
-        for(let i=0; i<amigos.length; i++){
+        for(let i=0; i<indiceMaximo; i++){
             // Agregar elementos a la lista: Para cada amigo, crear un nuevo elemento de lista.
-            let obtenerNombre = document.createElement("li");
+            let obtenerNombre = document.createElement('li');
             obtenerNombre.innerHTML = amigos[i];
             lista.appendChild(obtenerNombre);
-            console.log(lista);
         }
 
     }
@@ -73,13 +57,11 @@ function actualizarListaDeAmigos(){
     }
 }
 
-
-
 // Escribe una función que seleccione de manera aleatoria uno de los nombres 
 // almacenados en el array amigos.
 function sortearAmigo(){
     // Mostrar el resultado
-    if(amigos.length == 0){
+    if(indiceMaximo == 0){
         alert('No hay amigos disponibles para sortear');
         return;
     }
@@ -91,20 +73,15 @@ function sortearAmigo(){
     }
 }
 
-
 function generarIndiceAleatorio(){
     // Generar un índice aleatorio
     console.log('maximo:', indiceMaximo);
     let indiceAlteatorio = Math.floor(Math.random()*indiceMaximo);
     console.log('indice aleatorio:',indiceAlteatorio);
-    console.log(typeof(listaNombresSorteados.length));
-    console.log(typeof(indiceMaximo));
 
-    // Validar que haya amigos disponibles: Antes de sortear, comprobar si el 
-    // array amigos no está vacío.
+    // Validar que haya amigos disponibles
     if(listaNombresSorteados.length == indiceMaximo){
         alert('Ya se sortearon todos los nombres posibles.');
-        console.log(listaNombresSorteados[listaNombresSorteados.length -1]);
         return listaNombresSorteados[listaNombresSorteados.length -1];
     }
     else{
@@ -116,4 +93,17 @@ function generarIndiceAleatorio(){
         }
     }
     
+}
+
+function condicionesInciales(){
+    // Limpiar el campo de entrada
+    document.querySelector('#amigo').value = '';
+
+    //Al ingresar un nuevo nombre el sorteo de amigos se reinicia
+    listaNombresSorteados = [];
+    limpiarLista('resultado');
+}
+
+function limpiarLista(idLista){
+    return document.querySelector(`#${idLista}`).innerHTML = '';
 }
