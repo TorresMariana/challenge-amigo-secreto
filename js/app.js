77ffa38fca90368dd76da417b6b38e2e38bdcd63
TorresMariana <mt.grafico@gmail.com>
2025-03-07 22:31:34 -0300

    creación de la funcion actualizarListaDeAmigos

--- js/app.js
+++ js/app.js
@@ -7,6 +7,8 @@ let amigos = [];
 // Desarrolla una función, que permita al usuario ingresar un nombre en 
 // el campo de texto y añadirlo a la lista de amigos creada anteriormente.
 function agregarAmigo(){
+    // Capturar el valor del campo de entrada: Utilizar document.getElementById o 
+    // document.querySelector para obtener el texto ingresado por el usuario.
     let nombre = document.getElementById('amigo').value;
     console.log(nombre);
 
@@ -14,12 +16,13 @@ function agregarAmigo(){
     if(nombre == ''){
         alert('Por favor, inserte un nombre.');
     }else{
+        // Actualizar el array de amigos
         amigos.push(nombre);
-        limpiarEntrada();
+        // Limpiar el campo de entrada
+        document.querySelector('#amigo').value = '';
     }
 
     console.log(amigos);
-    obtenerNombres();
     actualizarListaDeAmigos();
 }
 
@@ -27,58 +30,41 @@ function limpiarEntrada(){
     document.querySelector('#amigo').value = '';
     return;
 }
-
-/*Capturar el valor del campo de entrada: Utilizar document.getElementById o 
-document.querySelector para obtener el texto ingresado por el usuario.
-
-Validar la entrada: Implementar una validación para asegurarse de que el campo 
-no esté vacío. Si está vacío, mostrar un alert con un mensaje de 
-error: "Por favor, inserte un nombre."
-
-Actualizar el array de amigos: Si el valor es válido, añadirlo al arreglo que 
-almacena los nombre de amigos usando el método.push().
-
-Limpiar el campo de entrada: Después de añadir el nombre, restablecer el 
-campo de texto a una cadena vacía.
-*/
+function limpiarLista(idLista){
+    return document.querySelector(`#${idLista}`).innerHTML = "";
+}
 
 
-
-/*
-Crea una función que recorra el array amigos y agregue cada nombre como 
-un elemento <li> dentro de una lista HTML. Usa innerHTML para limpiar la 
-lista antes de agregar nuevos elementos.
-
-Obtener el elemento de la lista: Utilizar document.getElementById() o 
-document.querySelector() para seleccionar la lista donde se mostrarán los amigos.
-
-Limpiar la lista existente: Establecer lista.innerHTML = "" para asegurarse 
-de que no haya duplicados al actualizar.
-
-Iterar sobre el arreglo: Usa un bucle for para recorrer el arreglo amigos 
-y crear elementos de lista (<li>) para cada título.
-
-Agregar elementos a la lista: Para cada amigo, crear un nuevo elemento de lista.
-*/
+// Crea una función que recorra el array amigos y agregue cada nombre como 
+// un elemento <li> dentro de una lista HTML. Usa innerHTML para limpiar la 
+// lista antes de agregar nuevos elementos.
 function actualizarListaDeAmigos(){
+    // Obtener el elemento de la lista
     let lista = document.getElementById('listaAmigos');
-    //Limpiar la lista para asegurar de que no haya duplicados
+
+    // Limpiar la lista existente
+   limpiarLista('listaAmigos');
     if(amigos.length > 0){
-        let elementoLi = document.createElement("li");
+        // Iterar sobre el arreglo: Usa un bucle for para recorrer el arreglo 
+        // amigos y crear elementos de lista (<li>) para cada título.
         for(let i=0; i<amigos.length; i++){
-            lista.innerHTML += elementoLi.innerHTML = amigos[i];
+            // Agregar elementos a la lista: Para cada amigo, crear un nuevo elemento de lista.
+            let obtenerNombre = document.createElement("li");
+            obtenerNombre.innerHTML = amigos[i];
+            lista.appendChild(obtenerNombre);
             console.log(lista);
-    }
+        }
+
     }
     else{
         return lista.innerHTML = "";
     }
 }
 
-function obtenerNombres(){
-    let elementoLi = document.createElement("li");
-    for(let i=0; i<amigos.length; i++){
-        elementoLi.innerHTML = amigos[i];
-        console.log(elementoLi);
-    }
-}
+// function obtenerNombres(){
+//     let elementoDeLista = document.createElement("li");
+//     for(let i=0; i<amigos.length; i++){
+//         elementoDeLista.innerHTML = amigos[i];
+//         console.log(elementoDeLista);
+//     }
+// }
